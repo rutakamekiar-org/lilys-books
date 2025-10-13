fb66be5231ed4b413b425a0dcffa3c1bc6edb777
Vladyslav Kovalov <Vladyslav.Kovalov@gunnebo.com>
2025-10-13 22:42:16 +0200

    fix build

--- src/app/analytics.tsx
+++ src/app/analytics.tsx
@@ -13,7 +13,7 @@ export default function Analytics() {
         const url = pathname + searchParams.toString()
 
         // Send a pageview to Google Analytics
-        // @ts-expect-error
+        // @ts-expect-error qwerewr
         window.gtag?.("config",
             GA_MEASUREMENT_ID, {
             page_path: url,
