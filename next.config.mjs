f3a1a315adddbcc143615db2085e9e8042ec3e2a
Vladyslav Kovalov <Vladyslav.Kovalov@gunnebo.com>
2026-09-04 21:30:22 +0300

    fix: allow API-hosted product images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zvychajna.pp.ua",
+        pathname: "/images/**",
+      },
+    ],
+  },
+};
 
 export default config;
